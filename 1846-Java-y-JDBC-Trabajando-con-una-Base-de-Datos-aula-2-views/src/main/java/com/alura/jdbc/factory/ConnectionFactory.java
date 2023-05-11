dceaa23883fa26115371afaf3cfd5efe74b3876f
Juan Carlos <juanvillarq0097@gmail.com>
2023-05-11 11:49:40 -0500

    Implementacion de pool de conexiones con cantidad max y min posibles. Prton de diseño C3P0 para manejar el pool de conexiones. agragadas a las dependencias c3p0 y mchange-commons-java,para agregar detalles de datasource via log de la aplicacion.  Creacion de nueva clase para prueba de conexiones,Creacion de nuevo package para encapsular el objeto producto, creacion de controller y calse productoDAO para dar acceso a las operaciones de la tabla productos en la base de datos,Creacion de nueva tabla en db para categoria de los producto, implementacion y conexion con tabla productos, para guardar segun categoria

--- 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,17 +1,35 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
-	public Connection recuperarConexion() throws SQLException {
+	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+		var poolDataSource = new ComboPooledDataSource();
+		poolDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		poolDataSource.setUser("root");
+		poolDataSource.setPassword("Encaminoaserdesarrollador");
+		poolDataSource.setMaxPoolSize(10);
 		
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-				"root",
-				"Encaminoaserdesarrollador");
+		this.datasource = poolDataSource;
+		
+	}
+	
+	public Connection recuperarConexion()  {
+		
+		try {
+			return this.datasource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 		
 	}
 	
--- 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/view/ControlDeStockFrame.java
+++ 1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/view/ControlDeStockFrame.java
@@ -20,6 +20,8 @@ import javax.swing.table.DefaultTableModel;
 
 import com.alura.jdbc.controller.CategoriaController;
 import com.alura.jdbc.controller.ProductoController;
+import com.alura.jdbc.modelo.Categoria;
+import com.alura.jdbc.modelo.Producto;
 
 public class ControlDeStockFrame extends JFrame {
 
@@ -27,7 +29,7 @@ public class ControlDeStockFrame extends JFrame {
 
     private JLabel labelNombre, labelDescripcion, labelCantidad, labelCategoria;
     private JTextField textoNombre, textoDescripcion, textoCantidad;
-    private JComboBox<Object> comboCategoria;
+    private JComboBox<Categoria> comboCategoria;
     private JButton botonGuardar, botonModificar, botonLimpiar, botonEliminar, botonReporte;
     private JTable tabla;
     private DefaultTableModel modelo;
@@ -99,11 +101,11 @@ public class ControlDeStockFrame extends JFrame {
         textoDescripcion = new JTextField();
         textoCantidad = new JTextField();
         comboCategoria = new JComboBox<>();
-        comboCategoria.addItem("Elige una Categoría");
+        comboCategoria.addItem(new Categoria(0,"Elige una Categoría"));
 
-        // TODO
+        
         var categorias = this.categoriaController.listar();
-        // categorias.forEach(categoria -> comboCategoria.addItem(categoria));
+        categorias.forEach(categoria -> comboCategoria.addItem(categoria));
 
         textoNombre.setBounds(10, 25, 265, 20);
         textoDescripcion.setBounds(10, 65, 265, 20);
@@ -185,32 +187,18 @@ public class ControlDeStockFrame extends JFrame {
 
         Optional.ofNullable(modelo.getValueAt(tabla.getSelectedRow(), tabla.getSelectedColumn()))
                 .ifPresentOrElse(fila -> {
-                	 Integer id = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 0).toString());
+                	Integer id = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 0).toString());
                     String nombre = (String) modelo.getValueAt(tabla.getSelectedRow(), 1);
                     String descripcion = (String) modelo.getValueAt(tabla.getSelectedRow(), 2);
                     Integer cantidad = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 3).toString());
 
-                    int filasModificadas;
+                    var filasModificadas = this.productoController.modificar(nombre, descripcion, cantidad, id);
                     
-                    try {
-						filasModificadas = this.productoController.modificar(nombre, descripcion, cantidad, id);
-					} catch (SQLException e) {
-						e.printStackTrace();
-						throw new RuntimeException(e);
-					}
-                    JOptionPane.showMessageDialog(this, filasModificadas + "%d Item Modificado con éxito!");
+                    JOptionPane.showMessageDialog(this, String.format("%d item modificado con éxito!", filasModificadas));
                 }, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
     }
 
-   /*
-    * 
-    * Tratar la excepcion de la query Eliminar/DELETE
-    * 1.declaramos la variable que almacenara el valor de eliminar id
-    * 2.Encapsulamos el metodo eliminar dentro de try/catch
-    * 3.Asignamos el metodo eliminar id a la variable declarada
-    * 4.Atrapamos la excepcion en una runtimeException
-    * 5.Al mostrar el mensaje de salida en la aplicacion agragamos la variable
-    */
+
     private void eliminar() {
         if (tieneFilaElegida()) {
             JOptionPane.showMessageDialog(this, "Por favor, elije un item");
@@ -221,35 +209,26 @@ public class ControlDeStockFrame extends JFrame {
                 .ifPresentOrElse(fila -> {
                     Integer id = Integer.valueOf(modelo.getValueAt(tabla.getSelectedRow(), 0).toString());
                     
-                    int cantidadEliminada;
-                    try {
-						cantidadEliminada = this.productoController.eliminar(id);
-					} catch (SQLException e) {
-						throw new RuntimeException(e);
-					}
-
+                    var cantidadEliminada = this.productoController.eliminar(id);
                     modelo.removeRow(tabla.getSelectedRow());
  
-                    JOptionPane.showMessageDialog(this, cantidadEliminada + " Item eliminado con éxito!");
+                    JOptionPane.showMessageDialog(this,
+                            String.format("%d item eliminado con éxito!", cantidadEliminada));
                 }, () -> JOptionPane.showMessageDialog(this, "Por favor, elije un item"));
     }
 
     private void cargarTabla() {
-        try {
-        	var productos = this.productoController.listar();
-        	 try {
+        var productos = this.productoController.listar();
                 
-                 productos.forEach(producto -> modelo.addRow(new Object[] {  producto.get("ID") ,
-                		 producto.get("NOMBRE"), producto.get("DESCRIPCION"), producto.get("CANTIDAD")}));
-                 
-             } catch (Exception e) {
-                 throw e;
-             }
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} 
+        productos.forEach(producto -> modelo.addRow(new Object[] {  producto.getId() ,
+                		 producto.getNombre(),
+                		 producto.getDescripcion(),
+                		 producto.getCantidad() }));
+              
     }
-
+    
+    
+    
     private void guardar() {
         if (textoNombre.getText().isBlank() || textoDescripcion.getText().isBlank()) {
             JOptionPane.showMessageDialog(this, "Los campos Nombre y Descripción son requeridos.");
@@ -266,24 +245,15 @@ public class ControlDeStockFrame extends JFrame {
             return;
         }
 
-        // Logica de INSERT,desde la aplicacion
-        var producto = new HashMap<String, String>();
-        producto.put("NOMBRE", textoNombre.getText());
-        producto.put("DESCRIPCION",textoDescripcion.getText());
-        producto.put("CANTIDAD", String.valueOf(cantidadInt));
         
-        var categoria = comboCategoria.getSelectedItem();
-
-        /*
-         * Tratar la excepcion de la query Guardar/INSERT
-         * 1.Encapsulamos el metodo guardar dentro de try/catch
-         * 2. atrapamos la excepcion en una runtimeException
-         */
-        try {
-			this.productoController.guardar(producto);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
+        var producto = new Producto(textoNombre.getText(),
+        		textoDescripcion.getText(),
+        		cantidadInt);
+        
+        var categoria = (Categoria) comboCategoria.getSelectedItem();
+	    
+        this.productoController.guardar(producto, categoria.getId());
+		
 
         JOptionPane.showMessageDialog(this, "Registrado con éxito!");
 
